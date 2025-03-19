fb402d4095c7d2aa55210e8cac03555a4d8b1089
Boris <shevnin.boris2@yandex.ru>
2025-03-19 19:34:57 +0300

    end of do_with_retry

--- internal/utils/do_with_retry.go
+++ internal/utils/do_with_retry.go
@@ -4,17 +4,13 @@ import (
 	"GitHubTask/internal/api"
 	"fmt"
 	"net/http"
+	"strconv"
 	"time"
 )
 
 // - Обратить внимание на заголовки ответа GitHub API:
 //    - **X-RateLimit-Remaining** – количество оставшихся запросов
 //    - **Retry-After** – рекомендуемая задержка перед следующим запросом в случае превышения лимита.
-
-// нужно отправить запрос через функцию DoWithRetry которая:
-// 1. Проверяет вернул ли запрос 429 ошибку
-// 2. Если ошибка есть, то запрос отправляется повторно по экспоненциальной штуке, и мы возвращаем ошибку и респонсе
-// 3. Если ошибки нет, то возвращаем nil и респонс
 
 func DoWithRetry(req *http.Request, maxRetry int) (*http.Response, error) {
 	delay := 1 * time.Second
@@ -23,32 +19,42 @@ func DoWithRetry(req *http.Request, maxRetry int) (*http.Response, error) {
 
 	for attempt := 1; attempt <= maxRetry; attempt++ {
 		response, respErr = api.GithubClient.Do(req)
-
+		// Если сетевая ошибка
 		if respErr != nil {
+			// Если попытка последняя
+			if attempt == maxRetry {
+				return nil, fmt.Errorf("последняя попытка исчерпана: %v", respErr)
+			}
+
 			time.Sleep(delay)
-			delay = delay * 2
-		} else {
-			return response, nil
+			delay *= 2
+			continue
 		}
 
-		if response != nil {
+		// Если сервер вернул 429
+		if response.StatusCode == http.StatusTooManyRequests {
 			if rateLimit := response.Header.Get("X-RateLimit-Limit"); rateLimit != "" {
-				fmt.Println("X-RateLimit-Limit", rateLimit)
+				fmt.Println("X-RateLimit-Limit:", rateLimit)
 			}
-			if rateRemaning := response.Header.Get("X-RateLimit-Remaining"); rateRemaning != "" {
-				fmt.Println("X-RateLimit-Remaining", rateRemaning)
+			if rateRemaining := response.Header.Get("X-RateLimit-Remaining"); rateRemaining != "" {
+				fmt.Println("X-RateLimit-Remaining:", rateRemaining)
 			}
 
 			if retryDelay := response.Header.Get("Retry-After"); retryDelay != "" {
-				fmt.Println("retryDelay", retryDelay)
-				parseDelay, parseError := time.ParseDuration(retryDelay + "s")
-				if parseError != nil {
-					fmt.Println("DoWithRetry, parse delay error:", parseError)
+				fmt.Println("Retry-After:", retryDelay)
+				if seconds, err := strconv.Atoi(retryDelay); err == nil {
+					delay = time.Duration(seconds) * time.Second
+				} else {
+					fmt.Println("DoWithRetry, parse delay error:", err)
 				}
-				delay = parseDelay
 			}
+			time.Sleep(delay)
+			continue
 		}
 
+		// Если ошибок нет – возвращаем ответ
+		return response, nil
 	}
-	return response, nil
+
+	return nil, fmt.Errorf("все попытки исчерпаны")
 }
