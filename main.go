3d8c3cc0ec2244a351d4dbc3acdf7d87cd7aa566
Boris <shevnin.boris2@yandex.ru>
2025-03-20 11:45:41 +0300

    add 200 repos

--- main.go
+++ main.go
@@ -15,8 +15,9 @@ import (
 // todo: добавить paging и адаптировать код под него
 // todo: сделать todo из файла api.go
 
-func fetchRepos() (models.RepositoryList, error) {
-	url := "https://api.github.com/search/repositories?q=language:Go&sort=stars&order=desc&per_page=100&page=1"
+func fetchRepos(page int) (models.RepositoryList, error) {
+	//url := "https://api.github.com/search/repositories?q=language:Go&sort=stars&order=desc&per_page=100&page=1"
+	url := fmt.Sprintf("https://api.github.com/search/repositories?q=language:Go&sort=stars&order=desc&per_page=100&page=%d", page)
 
 	req, reqErr := http.NewRequest(http.MethodGet, url, nil)
 	if reqErr != nil {
@@ -26,10 +27,11 @@ func fetchRepos() (models.RepositoryList, error) {
 	// Устанавливаем заголовки через общую функцию
 	api.SetHeaders(req)
 
-	resp, respErr := api.GithubClient.Do(req)
+	resp, respErr := utils.DoWithRetry(req, 10)
 	if respErr != nil {
 		return models.RepositoryList{}, fmt.Errorf("ошибка при выполнении запроса fetchRepos: %w", respErr)
 	}
+
 	defer resp.Body.Close()
 
 	result, readErr := io.ReadAll(resp.Body)
@@ -58,7 +60,6 @@ func fetchRepoById(id int) (models.Repository, error) {
 
 	resp, respErr := utils.DoWithRetry(req, 5)
 
-	//resp, respErr := api.GithubClient.Do(req)
 	if respErr != nil {
 		return models.Repository{}, fmt.Errorf("ошибка при выполнении запроса fetchRepoById: %w", respErr)
 	}
@@ -82,14 +83,21 @@ func main() {
 	var totalForks int      //Общее число форков
 	var totalStars int      //Суммарное количество звезд
 	var totalOpenIssues int //Всего открытых issues
+	var reposPagesSlice []models.RepositoryList
+	reposPageNumber := 2
 
-	repIdList, resErr := fetchRepos()
-	if resErr != nil {
-		fmt.Println("Ошибка получения ответа от запроса fetchRepos:", resErr)
-		return
+	for i := 1; i <= reposPageNumber; i++ {
+		res, resErr := fetchRepos(i)
+		if resErr != nil {
+			fmt.Println("Ошибка получения ответа от запроса fetchRepos:", resErr)
+			return
+		}
+		reposPagesSlice = append(reposPagesSlice, res)
 	}
 
-	fmt.Println("repIdList.Items", len(repIdList.Items))
+	for _, items := range reposPagesSlice {
+		fmt.Println("items \n\n\n", items)
+	}
 
 	var repositories []models.Repository
 
@@ -98,27 +106,30 @@ func main() {
 	wg := sync.WaitGroup{}
 	mutex := sync.Mutex{}
 
-	for i := 0; i < len(repIdList.Items); i++ {
-		wg.Add(1)
-		go func(index int) {
-			defer wg.Done()
-			// заполняем слот семафора
-			semaphore <- struct{}{}
-			// в конце освобождаем слот семафора
-			defer func() {
-				<-semaphore
-			}()
+	for _, page := range reposPagesSlice {
+		pageCopy := page // копируем, чтобы каждая горутина работала с уникальными данными
+		for i := 0; i < len(pageCopy.Items); i++ {
+			wg.Add(1)
+			go func(index int) {
+				defer wg.Done()
+				// заполняем слот семафора
+				semaphore <- struct{}{}
+				// в конце освобождаем слот семафора
+				defer func() {
+					<-semaphore
+				}()
 
-			repository, err := fetchRepoById(repIdList.Items[index].ID)
-			if err != nil {
-				fmt.Println("ошибка при выполнении запроса fetchRepoById внутри семафора в main", err)
-			}
+				repository, err := fetchRepoById(pageCopy.Items[index].ID)
+				if err != nil {
+					fmt.Println("ошибка при выполнении запроса fetchRepoById внутри семафора в main", err)
+				}
 
-			// записываем в слайс репозиториев репозиторий
-			mutex.Lock()
-			repositories = append(repositories, repository)
-			mutex.Unlock()
-		}(i)
+				// записываем в слайс репозиториев репозиторий
+				mutex.Lock()
+				repositories = append(repositories, repository)
+				mutex.Unlock()
+			}(i)
+		}
 	}
 
 	wg.Wait()
@@ -131,6 +142,6 @@ func main() {
 	}
 
 	fmt.Printf("Forks %d, Stars %d, OpenIssues %d \n", totalForks, totalStars, totalOpenIssues)
-	//fmt.Println("repositories", repositories)
+	fmt.Println("rep len", len(repositories))
 
 }
